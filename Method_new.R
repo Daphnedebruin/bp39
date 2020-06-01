dbe08c479c017ed60c16494e40dd944bad1f1005
Daphnedebruin <66276782+Daphnedebruin@users.noreply.github.com>
2020-06-01 22:18:01 +0200

    Delete Method_new.R

--- Method_new.R
+++
@@ -1,8 +0,0 @@
-WTPS <- function(formula,theData){
-  theRes <- GFD(formula, data = theData, nperm = 999, alpha = 0.05)
-  return(theRes$WTS[4])
-}
-Method_new<- function(SimData){
-  formula <- Y~group
-  res <- WTPS(formula, theData = SimData)
-  return <- res}
